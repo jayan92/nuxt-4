f89dcb5a9813f4e0807df357aaef9a7146aba987
Jayan <mail4jayanchinthaka@gmail.com>
2024-11-05 22:50:34 +0530

    first commit

---
+++ app/pages/about.vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+    <section class="container py-20">
+      <h1 class="text-6xl font-extrabold mb-6 text-balance">
+        Welcome to CuisineCraft:
+      </h1>
+
+      <p class="text-xl font-extrabold mb-6 text-balance max-w-screen-lg">
+        CuisineCraft is an innovative and user-friendly recipe application
+        designed to bring delicious and diverse recipes right to your
+        fingertips. Powered by the robust dummyJSON API, CuisineCraft provides a
+        wide array of culinary inspiration, making it easy to explore, save, and
+        follow recipes from various cuisines around the world. Whether you're a
+        seasoned chef or a home cook, CuisineCraft offers tools to personalize
+        your recipe discovery experience, helping you find exactly what you need
+        for any meal or occasion.
+      </p>
+    </section>
+  </main>
+</template>
+
+<script setup lang="ts"></script>
